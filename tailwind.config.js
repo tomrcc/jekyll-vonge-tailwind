c8340e58fdc2df52b827e917a2dffd290b9e02ea
tomrcc <tom@cloudcannon.com>
2023-12-19 15:29:11 +1300

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ 
+    './site/_includes/**/*.html',
+    './site/_layouts/**/*.html',
+    './site/*.md'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
